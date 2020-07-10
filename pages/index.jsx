4d1a057c7a049f58bd3b51c8823486ee613b2656
tataqt <tata_98@ukr.net>
2020-07-10 12:22:06 +0300

    SEO

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,14 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function index() {
     return <>
+        <Head>
+            <title>Next.js</title>
+            <meta name="keywords" content="next, js, react" />
+            <meta name="description" content="This is yt tutor for next" />
+            <meta charSet="UTF-8" />
+        </Head>
         <h1>Hello Next.js</h1>
         <p><Link href={'/about'}><a>About</a></Link></p>
         <p><Link href="/posts"><a>Posts</a></Link></p>
--- pages/posts.jsx
+++ pages/posts.jsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Posts() {
     return (
         <>
+            <Head>
+                <title>Post page |  Next course</title>
+            </Head>
             <h1>Posts</h1>
         </>
     )
