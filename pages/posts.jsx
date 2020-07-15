a5c6c2b99d24476a4c215e6e71fba43add3e87b0
tataqt <tata_98@ukr.net>
2020-07-15 12:32:17 +0300

    Api db mock

--- pages/posts.jsx
+++ pages/posts.jsx
@@ -1,13 +1,27 @@
 import Head from "next/head";
 import { MainLayout } from "../components/MainLayout";
+import { useState, useEffect } from 'react';
 
 export default function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function load() {
+            const response = await fetch('http://localhost:4200/posts');
+            const json = await response.json();
+            setPosts(json);
+        }
+        load();
+    }, [])
     return (
-        <>
-            <MainLayout title={"Posts page"}>
-                <title>Post page |  Next course</title>
-            </MainLayout>
-            <h1>Posts</h1>
-        </>
+
+        <MainLayout title={"Posts page"}>
+            <Head>
+                <title>Posts page |  Next course</title>
+            </Head>
+            <h1>Posts page</h1>
+            <pre>{JSON.stringify(posts, null, 2)}</pre>
+        </MainLayout>
+
     )
 }
