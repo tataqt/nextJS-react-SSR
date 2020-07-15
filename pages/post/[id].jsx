2abf491334b11f0015967d22cd1c7cfd30c0645f
tataqt <tata_98@ukr.net>
2020-07-15 14:44:41 +0300

    redner data api on server side getInitialProps

--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -1,10 +1,45 @@
-import { useRouter } from 'next/router';
 import { MainLayout } from '../../components/MainLayout';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import {useRouter} from 'next/router';
 
-export default function Post() {
+export default function Post({ post: serverPost }) {
+    const [post, setPost] = useState(serverPost);
     const router = useRouter();
 
-    return <MainLayout title={`Post page ${router.query.id} `}>
-        <h1>Post {router.query.id}</h1>
+    useEffect(() => {
+        async function load() {
+            const response = await fetch(`http://localhost:4200/posts/${router.query.id}`);
+            const data = await response.json();
+            setPost(data);
+        }
+
+        if (!serverPost) {
+            load();
+        }
+    }, []);
+
+    if (!post) {
+        return <MainLayout>
+            <h1>Loading...</h1>
+        </MainLayout>
+    }
+
+    return <MainLayout title={post.title}>
+        <h1>{post.title}</h1>
+        <hr />
+        <div>{post.body}</div>
+        <br />
+        <Link href={"/posts"}><a>Назад ко всем постам</a></Link>
     </MainLayout>
+}
+
+Post.getInitialProps = async ({ query, req }) => {
+    if (!req) {
+        return { post: null }
+    }
+
+    const response = await fetch(`http://localhost:4200/posts/${query.id}`);
+    const post = await response.json();
+    return { post };
 }
--- pages/posts.jsx
+++ pages/posts.jsx
@@ -1,27 +1,56 @@
 import Head from "next/head";
 import { MainLayout } from "../components/MainLayout";
-import { useState, useEffect } from 'react';
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
-export default function Posts() {
-    const [posts, setPosts] = useState([]);
+export default function Posts({ posts: serverPosts }) {
+    const [posts, setPost] = useState(serverPosts);
+    const router = useRouter();
 
     useEffect(() => {
         async function load() {
-            const response = await fetch('http://localhost:4200/posts');
-            const json = await response.json();
-            setPosts(json);
+            const response = await fetch(`http://localhost:4200/posts/`);
+            const data = await response.json();
+            setPost(data);
         }
-        load();
-    }, [])
-    return (
 
+        if (!serverPosts) {
+            load();
+        }
+    }, []);
+
+    if (!posts) {
+        return <MainLayout>
+            <h1>Loading...</h1>
+        </MainLayout>
+    }
+
+    return (
         <MainLayout title={"Posts page"}>
             <Head>
                 <title>Posts page |  Next course</title>
             </Head>
             <h1>Posts page</h1>
-            <pre>{JSON.stringify(posts, null, 2)}</pre>
-        </MainLayout>
-
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>
+                        <Link href={`/post/[id]`} as={`/post/${post.id}`}>
+                            <a>{post.title}</a>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </MainLayout >
     )
+}
+
+Posts.getInitialProps = async ({req}) => {
+    if (!req) {
+        return { posts: null }
+    }
+
+    const response = await fetch('http://localhost:4200/posts');
+    const posts = await response.json();
+    return { posts };
 }
