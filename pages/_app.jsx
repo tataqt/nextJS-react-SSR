8386aeb15c7ac22a2b9687aaa13911e7e40f59aa
tataqt <tata_98@ukr.net>
2020-07-16 09:57:32 +0300

    loading indicator

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,17 @@
 import styles from './../styles/main.css'
+import NextNprogress from 'nextjs-progressbar';
 
 export default function MyApp({
     Component,
     pageProps
 }) {
     return <>
+        <NextNprogress
+            color="yellow"
+            startPosition="0.3"
+            stopDelayMs="200"
+            height="3"
+        />
         <Component {...pageProps} />
         {/* <style global jsx>
             {`
